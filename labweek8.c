0f2a5842b573501f954d4704ac9e7c94fac3137e
Spwstop <suppawat.sa@ku.th>
2020-02-13 19:16:45 +0700

    Git basic

---
+++ labweek8.c
@@ -0,0 +1,11 @@
+//mkdir ____ สร้างไดเรคตอรี่
+//dir ดูรายการทั้งหมดใน files
+//cd ____ เปลี่ยนไปทำงานในเดเรคตอรี่ที่กำหนด
+//cd .. ย้อนกลับไปไดเรคตอรี่ก่อนหน้า
+//สร้างไฟล์ .git โดยใช้ git init
+//เพิ่มไฟล์ ที่จะ commit โดยใช้ git add "ชื่อไฟล์"
+//ตั้งค่าชื่อ ใช้ git config user.name / email ตามด้วยชื่อหรืออีเมล
+//commit ไฟล์เพื่อเก็บขั้นตอนโดยใช้ git commit -m "ชื่อ"
+//ดูที่ commit ทั้งหมดโดยใช้ git log --all --decorate --oneline --graph
+//เชื่อมกับ github โดย git remote add <-->
+//ส่งไปยัง github โดย git push -u origin master
