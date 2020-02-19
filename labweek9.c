6d350ab6cae990c91550914c134439cb87fcf027
Spwstop <suppawat.sa@ku.th>
2020-02-19 22:20:10 +0700

    Git branch

---
+++ labweek9.c
@@ -0,0 +1,6 @@
+//git clone <url> คัดลอก git 
+//git branch <name> สร้าง branch ใหม่
+//เช็คได้ด้วยการพิมพ์ git branch
+//ย้าย branch ด้วย git checkout <name>
+//merge โดยการเปลี่ยนไปที่ branch ที่จะให้เป็นหลัก
+//ใช้คำสั่ง git merge <name>
